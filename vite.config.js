c155e7791a0bb7a1fe9abac6649ed7fa92b5eb57
Ryan Collier <ryanhcollier@gmail.com>
2026-03-22 14:50:08 -0400

    fix: target _Fullsize dir and /bun/image cdn endpoint

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/remote-assets': {
+        target: 'https://reil.studio',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/remote-assets/, '/bun/image')
+      }
+    }
+  }
 })
